c911063fbd67af37eaf9053c195ec457cb886a8f
liz <coderliz1@gmail.com>
2017-11-13 02:14:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,19 @@ function logWhisper(string){
   return console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  if(string === string.toLowerCase()) return "I cant hear you!";
+//function sayHiToGrandma(string){
+  //if(string === string.toLowerCase()) return "I cant hear you!";
+//}
+
+//function sayHiToGrandma(string){
+//  if(string === string.toUpperCase()) return "YES INDEED!";
+//}
+//function sayHiToGrandma(string){
+  if(string === "I love you, Grandma.") return "I love you, too.";
 }
 
 function sayHiToGrandma(string){
-  if(string === string.toUpperCase()) return "YES INDEED!";
-}
-function sayHiToGrandma(string){
-  if(string === "I love you, Grandma.") return "I love you, too.";
-}
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+ else if (string === string.toUpperCase()) return "YES INDEED!";
+ }
